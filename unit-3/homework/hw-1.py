419e20415f094c0479ce6d756a3e9f7ad460ac64
Emma Danyliuk <emmadanyliuk@Emmas-MacBook-Pro-2.local>
2019-11-10 20:49:33 -0500

    homework for unit 3 revise with palindrome

--- unit-3/homework/hw-1.py
+++ unit-3/homework/hw-1.py
@@ -6,3 +6,17 @@ def emma_string(string):
     return result
 
 print(emma_string("EmmaEmma"))
+
+def palindrome(string):
+    result = ""
+    answer = ""
+    for character in string:
+        result = character + result
+        if string == result:
+            answer = True
+        else:
+            answer = False
+
+    return answer
+
+print(palindrome("racecar"))
